a5a4bbf0670430146497508a2e8f9a920049891d
q.chen.work <q.chen.work@outlook.com>
2023-11-14 18:14:06 +0800

    update

--- ui/fix-preload.js
+++ ui/fix-preload.js
@@ -4,7 +4,8 @@ const filePath =
   './node_modules/.pnpm/vite@2.9.16_sass@1.69.5/node_modules/vite/dist/node/chunks/dep-6e2fe41e.js';
 
 let content = fs.readFileSync(filePath, 'utf-8');
-
-content = content.replace('vite/preload-helper', '\0vite/preload-helper');
-
-fs.writeFileSync(filePath, content);
+const str = '\0vite/preload-helper';
+if (!content.includes(str)) {
+  content = content.replace('vite/preload-helper', '\0vite/preload-helper');
+  fs.writeFileSync(filePath, content);
+}
