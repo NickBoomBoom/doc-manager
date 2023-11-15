954ce58aff1057a722220430a4e5d36175b2b88a
q.chen.work <q.chen.work@outlook.com>
2023-11-15 17:10:06 +0800

    update

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -21,8 +21,8 @@ export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : process.env.VUE_ROUTER_MODE === 'history'
-    ? createWebHistory
-    : createWebHashHistory;
+      ? createWebHistory
+      : createWebHashHistory;
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
@@ -62,7 +62,7 @@ export default route(function (/* { store, ssrContext } */) {
           })
           .onCancel(() => {
             Router.replace({
-              name: 'Dashboard',
+              name: 'Doc',
             });
           });
         next(false);
