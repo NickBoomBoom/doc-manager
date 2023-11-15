0966816318b1785535a5e6983f2bd0e24519ce71
q.chen.work <q.chen.work@outlook.com>
2023-11-15 14:47:04 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,10 @@ newVersion=$(echo $currentVersion | awk -F. '{print $1 FS $2 FS $3+1}')
 sed -i '' "s/\"version\": \"$currentVersion\"/\"version\": \"$newVersion\"/" ./ui/package.json
 
 # 打标签并推送到远程仓库
+git add .
+git commit "update version $newVersion"
 git tag -a v$newVersion -m "Release $newVersion"
+git push
 git push --tags
 
 echo "Tag v$newVersion 已创建并推送到远程仓库。"
