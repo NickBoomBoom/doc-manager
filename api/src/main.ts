566f08ece8ffdc19b683d2d532bd86ea3abd0b4f
NickChen <Q7but@qq.com>
2024-03-25 14:57:38 +0800

    add  api doc

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { TransformInterceptor } from './common/interceptions/transform.intercept
 import * as morgan from 'morgan';
 import { HttpExceptionFilter } from './common/filters/http-execption.filters';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,18 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+  /* 启动swagger */
+  const options = new DocumentBuilder()
+    .addBearerAuth() // 开启 BearerAuth 授权认证
+    .setTitle('API 文档')
+    .setDescription('API 文档')
+    .setTermsOfService('https://docs.nestjs.cn/8/introduction')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  // 设置显示路由
+  SwaggerModule.setup('/api', app, document);
+
   await app.listen(configService.get('base.port'));
 }
 bootstrap();
